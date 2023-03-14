0bb9d900aa5cbd3648f20f637cd08bde5f950846
Luke Calderon <luke.a.calderon@gmail.com>
2023-03-14 15:54:10 -0500

    added session to login servlet

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,12 +3,22 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        HttpSession session = request.getSession(false); // don't create session if it doesn't exist
+        if (session != null && session.getAttribute("user") != null) {
+            // user is already logged in, redirect to profile
+            response.sendRedirect("/profile");
+        } else {
+            // user is not logged in, show login page
+            request.getRequestDispatcher("/login.jsp").forward(request, response);
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -17,9 +27,12 @@ public class LoginServlet extends HttpServlet {
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            HttpSession session = request.getSession(); // create new session
+            session.setAttribute("user", username); // set user key in session
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
         }
     }
 }
+
