507d7927cddc9ffcc0743f509eded0364a0b726d
Luke Calderon <luke.a.calderon@gmail.com>
2023-03-16 14:31:42 -0500

    finally fixed ad lister db

--- src/main/java/CreateAdServlet.java
+++ src/main/java/CreateAdServlet.java
@@ -13,11 +13,9 @@ public class CreateAdServlet extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        Ad ad = new Ad(
-            1, // for now we'll hardcode the user id
-            request.getParameter("title"),
-            request.getParameter("description")
-        );
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+        Ad ad = new Ad(1, title, description);
         DaoFactory.getAdsDao().insert(ad);
         response.sendRedirect("/ads");
     }
